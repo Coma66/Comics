649e70845fc53cdd70d0ca0f0d5100ce7fa00979
Chris Galler <gallercj@gmail.com>
2025-04-12 21:25:06 -0400

    Added execption handling, dialog boxes, and ability to choose starting picture number.

--- Picture_Retrieval.py
+++ Picture_Retrieval.py
@@ -1,6 +1,7 @@
 # Picture Storage Program
 # Chris Galler
-''' This file program takes a list of indexes from a csv and retrieves the corresponding pictures (only the first one) from local storage '''
+''' This file program takes a list of indexes from a csv and retrieves the corresponding pictures (only the first one) from local storage.
+It then stores those pictures into its own folder for easier retrieval. '''
 
 ''' Need to install pyzbar and cv2 packages from Command Shell
 pip install pyzbar
@@ -10,9 +11,11 @@ Check the Visual Studio C++ Distibutable if there are issues inporting pyzbar ''
 ######################
 # Import libraries
 ######################
+import tkinter.filedialog
 import cv2  # Read/Capture camera or images
 import os
 import csv
+import tkinter as tk
 
 #####################
 # Function definition
@@ -50,30 +53,42 @@ def getIndexNums(csvFile):
 
 def main():
     # Get parent directory from user
-    top = input('Please enter the directory in which the pictures are stored with the csv file: ')
+    #top = input('Please enter the directory in which the pictures are stored with the csv file: ')
+    top = tk.filedialog.askdirectory(title = 'Select a directory')
 
     # Change working directory to read csv file and retrieve pictures
     os.chdir(top)
 
-
     # Get the list of indexs from the .csv file
-    file_fold = input('Please enter the csv file name: ')
-    file = file_fold + '.csv'
+    file = tk.filedialog.askopenfilename(initialdir = top, title = 'Select a file', filetypes=(("CSV files","*.csv"), ("All files","*.*")))
+    folder = file.replace('.csv', '') # Make new folder based on CSV file name
     indexes, names, issues = getIndexNums(file)
 
     #  Make a new folder to store all pictures
-    newFold = os.path.join(top, file_fold)
-    os.mkdir(newFold)
+    newFold = os.path.join(top, folder)
+    try:
+        os.mkdir(newFold)
+    except FileExistsError:
+        print('Folder already exists, continuing on.')
 
     #  Initialize total picture counter, image list, new save name list, and which pic to grab from each index folder
-    picCount = 1
+    while True:
+        try:
+            picCount = int(input('Please enter the starting picture number for the sale folder: '))
+        except ValueError:
+            print("This isn't a valid number")
+        else:
+            break
     imageList = []
     imageName = []
     SOLD_PIC = 0
 
     #  Loop through the folders and copy the first picture in each folder to sold folder
     for i in range(0, len(indexes)):
-        os.chdir(os.path.join(top, indexes[i]))  #  Change the directory to the current index picture folder
+        try:
+            os.chdir(os.path.join(top, indexes[i]))  #  Change the directory to the current index picture folder
+        except FileNotFoundError:
+            print(f'File {indexes[i]} was not found, moving to next in the list.')
         pics = os.listdir()  #  Get the list of pictures in the folder
 
         #  Get first pic in folder
@@ -91,6 +106,9 @@ def main():
 
     #  Loop through images and save to new folder
     for k in range(0, len(imageList)):
-        cv2.imwrite(imageName[k], imageList[k])  #  Write the image to the new folder with a modified name
+        try:
+            cv2.imwrite(imageName[k], imageList[k])  #  Write the image to the new folder with a modified name
+        except cv2.error:
+            print(f'Image {imageName[k]} failed to write, moving to next image.')
 
 main()
